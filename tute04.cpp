62b7e10f06a4e3a4e6ecd67a8d18465256e531e9
IT21278280 <99583379+IT21278280@users.noreply.github.com>
2022-02-20 09:44:35 +0000

    What did you change

--- tute04.cpp
+++ tute04.cpp
@@ -27,3 +27,20 @@ int main() {
   std::cout << std::endl;
   return 0;
 }
+
+long Factorial(int no)
+{
+  long f;
+  f = 1;
+  for (int r=no; r >= 1; r--) 
+  {
+        f = f * r;
+  }
+  return f;
+}
+
+long nCr(int n, int r)
+{
+  return Factorial(n)/(Factorial(r)*Factorial(n-r)); 
+}
+
