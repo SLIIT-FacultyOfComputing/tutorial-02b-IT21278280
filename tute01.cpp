a8d3572080c34f556de3bfd5f389c3f35cfa5730
IT21278280 <99583379+IT21278280@users.noreply.github.com>
2022-02-20 10:30:13 +0000

    What did you change

--- tute01.cpp
+++ tute01.cpp
@@ -25,7 +25,7 @@ int main()
     std::cout <<"Enter a length in cm : ";
     std::cin >> cm;
     inches = cm / 2.54;
-    std::cout <<"Length in inches is y"<< inches;
+    std::cout <<"Length in inches is "<< inches;
     
     return 0;
 }  
